4115e1b896d5c61584f856e5c89dab834f808abb
Govind Kumar <govindkumar26112001@gmail.com>
2025-10-08 14:43:17 +0530

    selection sort

---
+++ selectionSort.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    // Function to perform selection sort on the given array.
+    void selectionSort(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        
+        for(int i=0 ; i<n ; i++){
+            int minIdx = i;
+            for(int j=i ; j<n ; j++){
+                if(arr[minIdx]>arr[j]){
+                    minIdx = j;
+                }
+            }
+            swap(arr[minIdx], arr[i]);
+        }
+    }
+};
