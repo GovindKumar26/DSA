18cafad5ef12f6f6d582263e0984ba18959d0c50
Govind Kumar <govindkumar26112001@gmail.com>
2025-10-08 15:29:42 +0530

    merge

---
+++ mergeSort.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  public:
+  
+    void merge(vector<int>&arr, int l, int mid, int r){
+        int i = l, j = mid+1;
+        
+        vector<int>temp(r-l+1, 0);
+        int k=0;
+        
+        while(i<=mid && j<=r){
+            if(arr[i]<arr[j])
+            temp[k++] = arr[i++];
+            else temp[k++] = arr[j++];
+        }
+        
+        while(i<=mid){
+            temp[k++] = arr[i++];
+        }
+        
+        while(j<=r){
+            temp[k++] = arr[j++];
+        }
+        
+        for(int k=0 ; k<temp.size() ; k++){
+            arr[l+k] = temp[k];
+        }
+    }
+  
+    void mergeSort(vector<int>& arr, int l, int r) {
+        
+        if(l==r){
+            return;
+        }
+        // code here
+        int mid = (l+r)/2;
+        
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1, r);
+        merge(arr, l, mid, r);
+    }
+};
